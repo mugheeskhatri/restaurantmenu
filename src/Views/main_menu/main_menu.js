ea252cc288d176b8b9bc8b060546f0a2b49d7bdd
Mughees-khatri <76055158+mugheeskhatri@users.noreply.github.com>
2022-07-04 23:29:22 -0700

    some changes

--- src/Views/main_menu/main_menu.js
+++ src/Views/main_menu/main_menu.js
@@ -171,7 +171,9 @@ const Index = () => {
             containerClass="carousel-container"
             removeArrowOnDeviceType={["tablet", "mobile"]}
             itemClass="carousel-item-padding-40-px"
-            afterChange={(e)=> setIndex(e- (imagesWithContent.length - 1))}
+            afterChange={(previous , {currentSlide})=> {
+                currentSlide < previous ? setIndex(index -1) : setIndex(index+1)
+            }}
           >
             {imagesWithContent.map((item, Index) => {
               return (
